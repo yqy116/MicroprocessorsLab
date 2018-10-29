ee59e194595ddb1c68be33401c1cf8904ef99806
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-10-29 11:44:31 +0000

    keypad new

--- LCD.asm
+++ LCD.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global  LCD_Setup, LCD_Write_Message,LCD_clear, LCD_2ndline
+    global  LCD_Setup, LCD_Write_Message,LCD_clear, LCD_2ndline, LCD_Send_Byte_D, LCD_delay_ms
 
 acs0    udata_acs   ; named variables in access ram
 LCD_cnt_l   res 1   ; reserve 1 byte for variable LCD_cnt_l
