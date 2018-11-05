1884de996023e2c55b368f00129e114269683cf5
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-05 15:49:00 +0000

    Not_so_random

--- Simple1.asm
+++ Simple1.asm
@@ -1,71 +1,65 @@
-	#include p18f87k22.inc
-
-	extern	UART_Setup, UART_Transmit_Message   ; external UART subroutines
-	extern  LCD_Setup, LCD_Write_Message	    ; external LCD subroutines
-	extern	LCD_Write_Hex			    ; external LCD subroutines
-	extern  ADC_Setup, ADC_Read		    ; external ADC routines
-	extern	calculation
+    #include p18f87k22.inc
+extern	LCD_Setup, LCD_Write_Message, LCD_Write_Hex, LCD_Send_Byte_D ,LCD_delay_ms
+acs0    udata_acs   ; named variables in access ram
+pos1	res 1
+pos2	res 1
+pos3	res 1
+pos4	res 1
+read_pos res 1
 	
-acs0	udata_acs   ; reserve data space in access ram
-counter	    res 1   ; reserve one byte for a counter variable
-delay_count res 1   ; reserve one byte for counter in the delay routine
+rst	code 0x0000 ; reset vector	
+	call LCD_Setup	
+	goto start
+int_hi	code 0x0008 ; high vector, no low vector
+	btfss INTCON,TMR0IF ; check that this is timer0 interrupt
+	retfie FAST ; if not then return
+	incf LATD ; increment PORTD
+	bcf INTCON,TMR0IF ; clear interrupt flag
+	retfie FAST ; fast return from interrupt
 
-tables	udata	0x400    ; reserve data anywhere in RAM (here at 0x400)
-myArray res 0x80    ; reserve 128 bytes for message data
-
-rst	code	0    ; reset vector
-	goto	setup
-
-pdata	code    ; a section of programme memory for storing data
-	; ******* myTable, data in programme memory, and its length *****
-myTable data	    "Hello World!\n"	; message, plus carriage return
-	constant    myTable_l=.13	; length of data
-	
 main	code
-	; ******* Programme FLASH read Setup Code ***********************
-setup	bcf	EECON1, CFGS	; point to Flash program memory  
-	bsf	EECON1, EEPGD 	; access Flash program memory
-	call	UART_Setup	; setup UART
-	call	LCD_Setup	; setup LCD
-	call	ADC_Setup	; setup ADC
-	goto	start
+start	clrf TRISD ; Set PORTD as all outputs
+	clrf LATD ; Clear PORTD outputs
+	movlw b'10000000' ; Set timer0 to 16-bit, Fosc/4/256
+	movwf T0CON ; = 62.5KHz clock rate, approx 1sec rollover
+	bsf INTCON,TMR0IE ; Enable timer0 interrupt
+	bsf INTCON,GIE ; Enable all interrupts
 	
-	; ******* Main programme ****************************************
-start 	lfsr	FSR0, myArray	; Load FSR0 with address in RAM	
-	movlw	upper(myTable)	; address of data in PM
-	movwf	TBLPTRU		; load upper bits to TBLPTRU
-	movlw	high(myTable)	; address of data in PM
-	movwf	TBLPTRH		; load high byte to TBLPTRH
-	movlw	low(myTable)	; address of data in PM
-	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	myTable_l	; bytes to read
-	movwf 	counter		; our counter register
-loop 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
-	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
-	decfsz	counter		; count down to zero
-	bra	loop		; keep going until finished
+check	setf	TRISE
+	movlw	0xff
+	movwf	0x01
+	CPFSEQ	PORTE
+	bra	check
+	;Start reading the values
+	call	read
+	movff	read_pos, pos1
+	call	read
+	movff	read_pos, pos2
+	call	read
+	movff	read_pos, pos3
+	call	read
+	movff	read_pos, pos4
+	;stop interupt
+	movlw	b'00000000'
+	movwf	T0CON
+	;move value to W for mask
+	movlw	0x03
+	ANDWF	pos1, f
+	ANDWF	pos2, f
+	ANDWF	pos3, f
+	ANDWF	pos4, f
+	clrf	TRISC
+	clrf	TRISH
+	movff	pos4, PORTC
+	movff	pos1, PORTH
+	
+	goto $ ; Sit in infinite loop
 		
-	movlw	myTable_l-1	; output message to LCD (leave out "\n")
-	lfsr	FSR2, myArray
-	;call	LCD_Write_Message
-
-	movlw	myTable_l	; output message to UART
-	lfsr	FSR2, myArray
-	;call	UART_Transmit_Message
-	call	calculation
+read	movff	PORTD, read_pos
+	call	LCD_delay_ms
+	return
 	
-;measure_loop
-;	call	ADC_Read	;taking 12bit and low sig bit to ADRESL, high bit to ADRESH
-;	movf	ADRESH,W
-;	call	LCD_Write_Hex
-;	movf	ADRESL,W
-;	call	LCD_Write_Hex
-;	goto	measure_loop		; goto current line in code
-
-	; a delay subroutine if you need one, times around loop in delay_count
-delay	decfsz	delay_count	; decrement until zero
+delay	decfsz 0x01 ; decrement until zero
 	bra delay
 	return
-
 	end
-	
