5336d3d4ca753cee67a64c1cff9cbee15a3ad0b5
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-12 16:21:14 +0000

    Not_so_random

--- Simple1.asm
+++ Simple1.asm
@@ -33,6 +33,18 @@ not_socorrect_count res 1
 not_socorrect_temp  res 1
 temp_res_2  res 1  
 game_counter res 1
+R_count	res 1
+G_count	res 1    
+Y_count res 1
+B_count res 1
+temp_store res 1
+B_count_seq res 1
+R_count_seq res 1
+Y_count_seq res 1
+G_count_seq res 1
+total_light res 1
+y_count	res 1 
+exponent res 1
  
  
 rst	code 0x0000 ; reset vector	
@@ -82,8 +94,13 @@ check	setf	TRISE
 	movwf	T0CON
 	
 	;intialise
-	movlw	0x03
+	movlw	0x05
 	movwf	game_counter
+	movlw	0x00
+	movwf	R_count_seq
+	movwf	G_count_seq
+	movwf	Y_count_seq
+	movwf	B_count_seq
 	lfsr    FSR2, myinitial
 	call	lookup				;initialise the lookup table
 	
@@ -101,6 +118,49 @@ check	setf	TRISE
 	movwf	POSTINC2
 	call	write	
 
+;count
+	movff	pos1, temp_store
+	movf	temp_store, W
+	call	colour_count_seq
+	movff	pos2, temp_store
+	call	colour_count_seq
+	movff	pos3, temp_store
+	call	colour_count_seq
+	movff	pos4, temp_store
+	call	colour_count_seq
+	goto	keyin
+	
+colour_count_seq
+	movlw	0x00
+	CPFSEQ	temp_store
+	goto	second_count_seq
+	movlw	0x01
+	addwf	R_count_seq, f
+	return
+	
+second_count_seq	
+	movlw	0x01
+	CPFSEQ	temp_store
+	goto	third_count_seq
+	movlw	0x01
+	addwf	G_count_seq, f
+	return
+	
+third_count_seq	
+	movlw	0x02
+	CPFSEQ	temp_store
+	goto	fourth_count_seq
+	movlw	0x01
+	addwf	B_count_seq, f
+	return	
+
+fourth_count_seq	
+	movlw	0x03
+	CPFSEQ	temp_store
+	return
+	movlw	0x01
+	addwf	Y_count_seq, f
+	return		
 	
 keyin	movlw b'10000000' ; Set timer0 to 16-bit, Fosc/4/256
 	movwf T0CON ; = 62.5KHz clock rate, approx 1sec rollover
@@ -109,6 +169,11 @@ keyin	movlw b'10000000' ; Set timer0 to 16-bit, Fosc/4/256
 	lfsr FSR0, myArray 
 	movlw	0x04
 	movwf	counter
+	movlw	0x00
+	movwf	R_count
+	movwf	G_count
+	movwf	Y_count
+	movwf	B_count
 	
 loop	movlw	0xff
 	CPFSEQ	tempo
@@ -116,10 +181,45 @@ loop	movlw	0xff
 	goto	loop
 	
 answ	movf	tempo, W
+	movff	tempo, temp_store
 	movff	tempo, POSTINC0
 	call	write
-	decfsz  counter
+	call	colour_count
+back	decfsz  counter
 	goto	loop
+	goto	initial
+	
+colour_count
+	movlw	0x77
+	CPFSEQ	temp_store
+	goto	second_count
+	movlw	0x01
+	addwf	R_count, f
+	return
+	
+second_count	
+	movlw	0xB7
+	CPFSEQ	temp_store
+	goto	third_count
+	movlw	0x01
+	addwf	G_count, f
+	return
+	
+third_count	
+	movlw	0xD7
+	CPFSEQ	temp_store
+	goto	fourth_count
+	movlw	0x01
+	addwf	B_count, f
+	return	
+
+fourth_count	
+	movlw	0xE7
+	CPFSEQ	temp_store
+	return
+	movlw	0x01
+	addwf	Y_count, f
+	return	
 	
 initial	;All kind of initialization
 	movlw	0x00
@@ -127,9 +227,11 @@ initial	;All kind of initialization
 	movwf	ans_pos
 	movwf	correct_count
 	movwf	temp_res
+	movwf	y_count
 	
 	movlw	0x04
 	movwf	pos_counter ;number of loop
+	movwf	total_light
 	movlw	0x05
 	movwf	not_socorrect_count
 	call	LCD_Clear
@@ -161,40 +263,44 @@ testtest
 	call	small_loop
 	decfsz  pos_counter 
 	goto	pos_chk
+	call	comparison
+	movf	temp_store,W
+	addwf	y_count,W
+	subwf	total_light, f
+	movf	total_light, W
+	movlw	0x01
+	addwf	total_light,f
+	call	add_z
 	clrf	TRISC
 	movff	temp_res, PORTC
-	goto	$
-	;goto	backgame
-	
-;back_game
-;	movlw	.5	    ;Need some time before it clear
-;	call	LCD_delay_ms
-;	call	LCD_Clear
-;	movlw	.5	    ;Need some time before it clear
-;	call	LCD_delay_ms
-;	decfsz	game_counter
-;	goto	keyin
-;	goto	$
-	
-wro_pos movlw	'Z'
-	;call	LCD_Send_Byte_D	
-	movlw	0x01
-	CPFSEQ	not_socorrect_temp
-	call	logic
-	return
+	goto	back_game
 
-logic	movlw	0x01
+back_game
+	movlw	.5	    ;Need some time before it clear
+	call	LCD_delay_ms
+	call	LCD_Clear
+	movlw	.5	    ;Need some time before it clear
+	call	LCD_delay_ms
+	decfsz	game_counter
+	goto	keyin
+	goto	$
+	
+add_z	decfsz	total_light
+	goto	binary_z
+	return
+	
+binary_z 
+	movlw	0x01
 	movwf	temp_scr
-	movwf	not_socorrect_temp
-	movff	not_socorrect_count, temp_pst
-	;addwf	temp_res
-	movf	not_socorrect_count, W
-	addlw	0x30 
-	call	LCD_Send_Byte_D
+	movff	total_light,exponent
+	movlw	0x04
+	addwf	exponent,f
+	movff	exponent,temp_pst
 	call	iter
 	movf	temp_scr, W
-	addwf	temp_res
-	return
+	addwf	temp_res, f
+	goto	add_z	
+	
 ;EVERYTHING HERE ONWARDS IS SUBROUTINE
 small_loop  	
 	movlw	0x01
@@ -230,6 +336,7 @@ cor_pos	movlw	'Y'
 	call	LCD_Send_Byte_D	
 	;movff	ran_pos, temp_pst
 	movlw	0x01
+	addwf	y_count
 	movwf	temp_scr
 	addwf	correct_count
 	movff	correct_count,temp_pst
@@ -237,8 +344,9 @@ cor_pos	movlw	'Y'
 	movf	temp_scr, W
 	addwf	temp_res
 	return
-
-
+	
+wro_pos movlw	'Z'
+	return
 	
 iter	decf	temp_pst
 	movlw	0x00
@@ -258,6 +366,43 @@ mutiplier   movlw   0x02
 	    MULWF   temp_scr
 	    movff   PRODL, temp_scr
 	    return
+
+comparison
+	movlw	0x00	
+	movwf	temp_store
+	
+	movlw	0x00
+	movf	R_count_seq, W
+	;CPFSEQ	R_count
+	CPFSLT	R_count
+	subwf	R_count, W
+	CPFSLT	R_count
+	addwf	temp_store, f
+	
+	movlw	0x00
+	movf	G_count_seq, W
+	;CPFSEQ	G_count
+	CPFSLT	G_count
+	subwf	G_count, W
+	CPFSLT	G_count
+	addwf	temp_store, f
+	
+	movlw	0x00
+	movf	Y_count_seq, W
+	;CPFSEQ	Y_count
+	CPFSLT	Y_count
+	subwf	Y_count, W
+	CPFSLT	Y_count
+	addwf	temp_store, f
+	
+	movlw	0x00
+	movf	B_count_seq, W
+	;CPFSEQ	B_count
+	CPFSLT	B_count
+	subwf	B_count, W
+	CPFSLT	B_count
+	addwf	temp_store, f
+	return
 	
 ;CORRECT CODE
 fair	call	read
