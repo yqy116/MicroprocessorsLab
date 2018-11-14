c666176219e84df7dc6c73d5549cf7ace546d2dd
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-14 13:54:22 +0000

    Not_so_random

---
+++ keyboard.asm
@@ -0,0 +1,37 @@
+#include p18f87k22.inc
+	extern	delay
+	global	tempo, keyboard
+	
+acs0	    udata_acs
+adder	res 1
+tempo	res 1	
+acs_ovr	access_ovr	
+	
+key	code	
+	
+keyboard	;banksel cannot be same line with a label,etc.start
+	banksel PADCFG1				;enable pull-ups and all that for PORTE
+	bsf	PADCFG1,RJPU,BANKED
+	movlw	0xFF
+	movwf	0x01
+	;Start the row 
+	movlw	0x0F				;gets the row byte
+	movwf	TRISJ
+	call	delay
+	movwf	PORTJ
+	call	delay
+	movf	PORTJ, W, ACCESS
+	movwf   adder
+	;Start the column			;gets the column byte
+	call	delay
+	movlw	0xF0
+	movwf	TRISJ
+	call	delay
+	movwf	PORTJ
+	call	delay
+	movf	PORTJ, W, ACCESS		;combine the row and column binary stuff and output it to portD
+	iorwf	adder, W
+	movwf	tempo
+	return
+	
+end
