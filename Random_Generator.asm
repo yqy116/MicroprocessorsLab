69ec8e2ad196fcc8ae93d48c1ac27136caf95471
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-14 13:39:07 +0000

    Not_so_random

--- Random_Generator.asm
+++ Random_Generator.asm
@@ -1,31 +1,67 @@
 #include p18f87k22.inc
-global	fair
-extern	LCD_Setup
-
-    
-acs0    udata_acs     
-dig_1	res 1
-dig_2	res 1
-read_pos res 1
+	extern	LCD_delay_ms, LCD_Send_Byte_D
+	global	storage,lookup, write
+	
+acs0	    udata_acs
+storage	    res 1 
 acs_ovr	access_ovr
 
+match	code	
 	
+lookup
+	movlb	6		    ;select bank 6
+	lfsr	FSR1, 0x680	    ;point FSR1 to the middle of bank 6
+	movlw	'R'		    ; load all of the ascii codes into locations +/- away from the FSR1
+	movwf	storage
+	movlw	0x00
+	movff	storage, PLUSW1
 	
+	movlw	'G'
+	movwf	storage
+	movlw	0x01
+	movff	storage, PLUSW1
 	
-read	movff	PORTD, read_pos
-	call	LCD_delay_ms
-	return
-	
-fair	call	read
-	movff	read_pos, dig_2
-	call	read
-	movff	read_pos, dig_1
-	movlw	0x01 ;masking
-	ANDWF	dig_2, f
-	ANDWF	dig_1, f
+	movlw	'B'
+	movwf	storage
 	movlw	0x02
-	MULWF	dig_2, W
-	movf	PRODL, W
-	ADDWF	dig_1, f
+	movff	storage, PLUSW1
+	
+	movlw	'Y'
+	movwf	storage
+	movlw	0x03
+	movff	storage, PLUSW1
+	
+	;keyin table
+	movlw	'R'		    ; load all of the ascii codes into locations +/- away from the FSR1
+	movwf	storage
+	movlw	0x77
+	movff	storage, PLUSW1
+	
+	movlw	'G'
+	movwf	storage
+	movlw	0xB7
+	movff	storage, PLUSW1
+	
+	
+	movlw	'B'
+	movwf	storage
+	movlw	0xD7
+	movff	storage, PLUSW1
+	
+	movlw	'Y'
+	movwf	storage
+	movlw	0xE7
+	movff	storage, PLUSW1
 	
 	return
+
+	
+write	
+	movff	PLUSW1, storage
+	movf	storage, W
+	call	LCD_Send_Byte_D			;once it's all retrieved, write it to the LCD
+	call	LCD_delay_ms
+	call	LCD_delay_ms
+	return	
+	
+end
