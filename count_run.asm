0bfac02b6b91c4cba93dc3edc5070a2c6d2f084a
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-14 17:35:40 +0000

    Not_so_random

--- count_run.asm
+++ count_run.asm
@@ -1,7 +1,7 @@
 #include p18f87k22.inc
 	global	count
 	extern	colour_count_seq,R_count_seq,G_count_seq,Y_count_seq,B_count_seq
-	extern	temp_store,pos1,pos2,pos3,pos4
+	extern	temp_store,pos1,pos2,pos3,pos4,R_count,G_count,Y_count,B_count
 
 	
 
