7351db24f7957f7f25f049a66d682d0ac30bf95b
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-14 17:45:09 +0000

    Not_so_random

--- count_run.asm
+++ count_run.asm
@@ -1,7 +1,7 @@
 #include p18f87k22.inc
 	global	count
 	extern	colour_count_seq,R_count_seq,G_count_seq,Y_count_seq,B_count_seq
-	extern	temp_store,pos1,pos2,pos3,pos4,R_count,G_count,Y_count,B_count
+	extern	temp_store,pos1,pos2,pos3,pos4
 
 	
 
@@ -12,10 +12,7 @@ colour_ini
 	movwf	G_count_seq
 	movwf	Y_count_seq
 	movwf	B_count_seq
-	movwf	R_count
-	movwf	G_count
-	movwf	Y_count
-	movwf	B_count
+
 	return
 	
 count	call	colour_ini
