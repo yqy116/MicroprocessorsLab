4ebb2fd5c3cc267ee21bc560932c881a0dac578c
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-14 13:54:27 +0000

    Not_so_random

---
+++ count_colour.asm
@@ -0,0 +1,110 @@
+#include p18f87k22.inc
+	global	temp_store,colour_count_seq,colour_count, comparison
+	global	R_count,G_count,Y_count,B_count,R_count_seq,G_count_seq,Y_count_seq,B_count_seq
+acs0	    udata_acs
+R_count	res 1
+G_count	res 1    
+Y_count res 1
+B_count res 1
+B_count_seq res 1
+R_count_seq res 1
+Y_count_seq res 1
+G_count_seq res 1
+temp_store  res 1
+acs_ovr	access_ovr	
+	
+match_colour code	
+	
+colour_count_seq
+	movlw	0x00
+	CPFSEQ	temp_store
+	goto	second_count_seq
+	movlw	0x01
+	addwf	R_count_seq, f
+	return
+	
+second_count_seq	
+	movlw	0x01
+	CPFSEQ	temp_store
+	goto	third_count_seq
+	movlw	0x01
+	addwf	G_count_seq, f
+	return
+	
+third_count_seq	
+	movlw	0x02
+	CPFSEQ	temp_store
+	goto	fourth_count_seq
+	movlw	0x01
+	addwf	B_count_seq, f
+	return	
+
+fourth_count_seq	
+	movlw	0x03
+	CPFSEQ	temp_store
+	return
+	movlw	0x01
+	addwf	Y_count_seq, f
+	return	
+
+colour_count
+	movlw	0x77
+	CPFSEQ	temp_store
+	goto	second_count
+	movlw	0x01
+	addwf	R_count, f
+	return
+	
+second_count	
+	movlw	0xB7
+	CPFSEQ	temp_store
+	goto	third_count
+	movlw	0x01
+	addwf	G_count, f
+	return
+	
+third_count	
+	movlw	0xD7
+	CPFSEQ	temp_store
+	goto	fourth_count
+	movlw	0x01
+	addwf	B_count, f
+	return	
+
+fourth_count	
+	movlw	0xE7
+	CPFSEQ	temp_store
+	return
+	movlw	0x01
+	addwf	Y_count, f
+	return	
+	
+comparison
+	movlw	0x00	
+	movwf	temp_store
+
+	movf	R_count_seq, W
+	CPFSLT	R_count
+	subwf	R_count, W
+	CPFSLT	R_count
+	addwf	temp_store, f
+	
+	movf	G_count_seq, W
+	CPFSLT	G_count
+	subwf	G_count, W
+	CPFSLT	G_count
+	addwf	temp_store, f
+	
+	movf	Y_count_seq, W
+	CPFSLT	Y_count
+	subwf	Y_count, W
+	CPFSLT	Y_count
+	addwf	temp_store, f
+	
+	movf	B_count_seq, W
+	CPFSLT	B_count
+	subwf	B_count, W
+	CPFSLT	B_count
+	addwf	temp_store, f
+	return	
+end
