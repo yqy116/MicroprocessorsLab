ad440b2e010da1811725a691b837cbc4e98abb16
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-14 14:27:14 +0000

    Not_so_random

---
+++ random_generate.asm
@@ -0,0 +1,32 @@
+#include p18f87k22.inc
+	global	fair,read
+	global	dig_1
+
+
+acs0	    udata_acs
+int_ct	    res 1
+read_pos    res 1
+dig_2	    res 1
+dig_1	    res 1
+acs_ovr	access_ovr	
+	
+generate    code
+    
+fair	call	read
+	movff	read_pos, dig_2
+	call	read
+	movff	read_pos, dig_1
+	movlw	0x01 ;masking
+	ANDWF	dig_2, f
+	ANDWF	dig_1, f
+	movlw	0x02
+	MULWF	dig_2, W
+	movf	PRODL, W
+	ADDWF	dig_1, f
+	return
+	
+read	movff	int_ct, read_pos
+	call	LCD_delay_ms
+	return
+	
+end
