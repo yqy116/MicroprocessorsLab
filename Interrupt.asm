f986214fa6bed956a48c4e4392a355bf155c5a93
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-15 19:23:33 +0000

    History Result thought uart

---
+++ Interrupt.asm
@@ -0,0 +1,32 @@
+#include p18f87k22.inc
+	global	interrupt_setup
+	extern	int_ct,keyboard
+	
+int_hi	code 0x0008 ; high vector, no low vector
+	btfss INTCON,TMR0IF ; check that this is timer0 interrupt
+	goto	 second
+	movlw	.1
+	movwf	TMR0L
+	incf	int_ct
+	bcf INTCON,TMR0IF ; clear interrupt flag
+	retfie FAST ; fast return from interrupt
+
+second	btfss PIR1,TMR1IF
+	retfie FAST
+	call	keyboard
+	bcf PIR1,TMR1IF ; clear interrupt flag
+	retfie FAST ; fast return from interrupt	
+	
+code
+	
+interrupt_setup
+	movlw b'00000100' ; Close timer 1, when repeat apparently not closed
+	movwf T1CON 
+	movlw b'10000000' ; Set timer0 to 16-bit, Fosc/4/256
+	movwf T0CON ; = 62.5KHz clock rate, approx 1sec rollover
+	bsf INTCON,TMR0IE ; Enable timer0 interrupt
+	bsf INTCON,GIE ; Enable all interrupts
+	bsf INTCON,PEIE 
+	
+	
+end
