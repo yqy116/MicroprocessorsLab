4a3f6455001d4b998a22b6ab02b2470ff4f195bb
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-15 19:23:29 +0000

    History Result thought uart

--- Simple1.asm
+++ Simple1.asm
@@ -13,6 +13,7 @@
 	extern	UART_Setup, UART_Transmit_Message
 	global	int_ct,myArray, myinitial, count_orange
 	extern	UART_loop
+	extern	interrupt_setup
 	
 acs0    udata_acs   ; named variables in access ram
 int_ct	res 1
@@ -27,20 +28,6 @@ read_count  res	1
 rst	code 0x0000 ; reset vector	
 	call LCD_Setup	
 	goto start
-int_hi	code 0x0008 ; high vector, no low vector
-	btfss INTCON,TMR0IF ; check that this is timer0 interrupt
-	goto	 second
-	movlw	.1
-	movwf	TMR0L
-	incf	int_ct
-	bcf INTCON,TMR0IF ; clear interrupt flag
-	retfie FAST ; fast return from interrupt
-
-second	btfss PIR1,TMR1IF
-	retfie FAST
-	call	keyboard
-	bcf PIR1,TMR1IF ; clear interrupt flag
-	retfie FAST ; fast return from interrupt
 	
 main	code
 start	call UART_Setup
@@ -49,14 +36,7 @@ start	call UART_Setup
 	clrf LATE
 	clrf LATC
 	clrf LATH
-	movlw b'00000100' ; Close timer 1, when repeat apparently not closed
-	movwf T1CON 
-	movlw b'10000000' ; Set timer0 to 16-bit, Fosc/4/256
-	movwf T0CON ; = 62.5KHz clock rate, approx 1sec rollover
-
-	bsf INTCON,TMR0IE ; Enable timer0 interrupt
-	bsf INTCON,GIE ; Enable all interrupts
-	bsf INTCON,PEIE 
+	call	interrupt_setup
 	call	startgame
 	call	loop_end
 	call	print
