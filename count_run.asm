6b107341e3cd1fdcb977963abfb97d19997d77b1
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-15 18:48:52 +0000

    History Result thought uart

--- count_run.asm
+++ count_run.asm
@@ -1,7 +1,7 @@
 #include p18f87k22.inc
 	global	count
 	extern	colour_count_seq,R_count_seq,G_count_seq,Y_count_seq,B_count_seq
-	extern	temp_store
+	extern	temp_store, myinitial
 
 	
 
@@ -15,7 +15,8 @@ colour_ini
 
 	return
 	
-count	call	colour_ini
+count	lfsr    FSR2, myinitial
+	call	colour_ini
 	movff	POSTINC2, temp_store
 	call	colour_count_seq
 	movff	POSTINC2, temp_store
