d5fadb2d5cfabd8fa4ce6197e9252bbc89c43d48
Kelvin Yu <Kelvin Yu@Yu>
2018-11-16 21:29:05 +0000

    commented

--- end_start.asm
+++ end_start.asm
@@ -4,32 +4,32 @@
 	global	startgame, endgame, wingame, buzzer, print_answer
 	    
 acs0	    udata_acs
-word_count  res 1 
-end_mssg res 15
+word_count  res 1 ;number of words to be key in lcd
+mssg res 15	  ;store the message
 acs_ovr	access_ovr
 
-input	code
+start_end_screen	code	;this contain all the subroutine to send end/start game messsage to LCD
 	
-startTable data	    "Start:Press E"	; message, plus carriage return
-winTable data	    "You win!"	; message, plus carriage return		
-myTable data	    "You lose!Ans is:"	; message, plus carriage return
+startTable data	    "Start:Press E"	; start message
+winTable data	    "You win!"	; win message
+myTable data	    "You lose!Ans is:"	;ending message
  
 startgame	
-	lfsr	FSR0, end_mssg	; Load FSR0 with address in RAM	
+	lfsr	FSR0, mssg	; Load FSR0 with address in RAM	
 	movlw	upper(startTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(startTable)	; address of data in PM
 	movwf	TBLPTRH		; load high byte to TBLPTRH
 	movlw	low(startTable)	; address of data in PM
 	movwf	TBLPTRL		; load low byte to TBLPTRL
-	movlw	.13
-	movwf	temp_store
+	movlw	.13		
+	movwf	temp_store	;store number of bytes of message into mssg
 	movlw	0x0d
-	movwf	word_count
+	movwf	word_count	;number of words to be key in lcd
 	goto	loop_end
  
 endgame	
-	lfsr	FSR0, end_mssg	; Load FSR0 with address in RAM	
+	lfsr	FSR0, mssg	; Load FSR0 with address in RAM	
 	movlw	upper(myTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(myTable)	; address of data in PM
@@ -37,13 +37,13 @@ endgame
 	movlw	low(myTable)	; address of data in PM
 	movwf	TBLPTRL		; load low byte to TBLPTRL
 	movlw	.16
-	movwf	temp_store
+	movwf	temp_store	;store number of bytes of message into mssg
 	movlw	0x10
-	movwf	word_count
+	movwf	word_count	;number of words to be key in lcd
 	goto	loop_end
 
 wingame	
-	lfsr	FSR0, end_mssg	; Load FSR0 with address in RAM	
+	lfsr	FSR0, mssg	; Load FSR0 with address in RAM	
 	movlw	upper(winTable)	; address of data in PM
 	movwf	TBLPTRU		; load upper bits to TBLPTRU
 	movlw	high(winTable)	; address of data in PM
@@ -51,7 +51,7 @@ wingame
 	movlw	low(winTable)	; address of data in PM
 	movwf	TBLPTRL		; load low byte to TBLPTRL
 	movlw	.8
-	movwf	temp_store
+	movwf	temp_store	;store number of bytes of message into mssg
 	movlw	0x08
 	movwf 	word_count	
 	goto	loop_end
@@ -61,25 +61,27 @@ loop_end
 	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
 	decfsz	temp_store	; count down to zero
 	bra	loop_end	; keep going until finished	
-	lfsr	FSR2, end_mssg
+	lfsr	FSR2, mssg
 	goto	print	
 	
-print	movf	POSTINC2, W
-	call	LCD_Send_Byte_D
-	decfsz	word_count
-	goto	print
+print	movf	POSTINC2, W	;move by one memory address after moving the content to W
+	call	LCD_Send_Byte_D	;send the byte to lcd screen
+	decfsz	word_count	;count down the number of bytes to be key in
+	goto	print		;repeat until finish displaying all the message in lcd
 	return	
 
-buzzer	movlw	0x01
-	movwf	PORTD
+;a subroutine to send a square wave for the buzzer	
+buzzer	movlw	0x01		
+	movwf	PORTD		;this will be connected to the buzzer
 	movlw	0xf0
+	call	LCD_delay_ms	;let the buzzer to last longer
 	call	LCD_delay_ms
-	call	LCD_delay_ms
-	clrf	PORTD
+	clrf	PORTD		;close the buzzer
 	return	
 
+;a subroutine to send the answer to LCD
 print_answer
-	movf	POSTINC2, W
+	movf	POSTINC2, W	
 	call	write
 	return
 	end
