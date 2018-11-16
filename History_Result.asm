2223c4f5f879a46974542c56489b0613d036bedb
Kelvin Yu <Kelvin Yu@Yu>
2018-11-16 23:56:15 +0000

    commented

--- History_Result.asm
+++ History_Result.asm
@@ -3,23 +3,23 @@
     extern	storage, y_count, count_orange, UART_Transmit_Byte,temp_store
     global	UART_loop
 	
-	
+;this code contain all the code to send the attempts and result to UART	
 history	code
 
 UART_loop
 	call	restart
-	decfsz	temp_store
+	decfsz	temp_store  ;loop four times to send all foru guess
 	goto	UART_loop
 	call	result
 	return
 restart	
-	movf    POSTINC2, W
+	movf    POSTINC2, W	;as fsr2 contain untranslated guess need to be translted before send to UART
 	movff	PLUSW1, storage
 	movf	storage, W
 	call    UART_Transmit_Byte
 	return	
 
-result	movlw	' '
+result	movlw	' '	;show result, how many green/amber
 	call    UART_Transmit_Byte
 	movf	y_count,W
 	addlw	0x30
