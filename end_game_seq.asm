be10abfc38ed0cfd71d30c18ce2731dcac60f0c8
Kelvin Yu <Kelvin Yu@Yu>
2018-11-17 00:02:25 +0000

    commented

--- end_game_seq.asm
+++ end_game_seq.asm
@@ -5,7 +5,7 @@
 ;this code contain how to determine the end game result(win/lose) and repeat the game for several tries
 ending_the_game	code
 	
-end_game_logic	
+end_game_logic	    ;determine win/lose
 	movlw	0x03
 	CPFSGT	y_count	    ;lose condition as win when y=4
 	call	buzzer	    ;buzzer sound if wrong
@@ -16,36 +16,36 @@ end_game_logic
 	movlw	0x04
 	CPFSEQ	y_count	    ;win condition
 	goto	back_game   ;wrong, try again until game tries are used up
-	call	wingame	    ;print winning message
+	call	wingame	    ;print winning message (end_start)
 	goto	replay
 
 back_game
 	call	LCD_Clear   ;clear the previos guess in LCD
 	decfsz	game_counter
-	goto	retry
-	call	endgame
+	goto	retry	    ;go back to key in guess until game tries used up
+	call	endgame	   ;print ending message (end_start
 	movlw	0x04
-	movwf	temp_store
+	movwf	temp_store  ;(to print the correct answer)
 	lfsr	FSR2,myinitial
-	call	secondline
+	call	secondline  ;move to second line
 
-show	call	print_answer
+show	call	print_answer	;print the correct answer
 	decfsz	temp_store
 	goto	show
 	goto	replay
 
-replay	movlw	0x7E	;loop the game again
+replay	movlw	0x7E	;loop the game again if pressed A
 	CPFSEQ	tempo
 	goto	replay
-	call	LCD_Clear
-	clrf	PORTH 
-	movlw	'\n'
+	call	LCD_Clear   ;clear the game
+	clrf	PORTH	    ;clear the LED light
+	movlw	'\n'	    ;send an extra empty line to separate past result with the new game result
 	call    UART_Transmit_Byte
 	return	
 
-retry	call	keyin
-	call	initial
-	call	after_y
-	goto	end_game_logic
+retry	call	keyin	    ;allow the guess again
+	call	initial	    ;from initialization to green(yes) calculation (light calculation)
+	call	after_y	    ;from amber light to show result in port H	(light calculation)
+	goto	end_game_logic	;determine win/lose
 	
 	end
