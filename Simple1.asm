b3d42b67aa834333fdd561c2538f657c335060df
Kelvin Yu <Kelvin Yu@Yu>
2018-11-17 00:02:43 +0000

    commented

--- Simple1.asm
+++ Simple1.asm
@@ -28,60 +28,60 @@ rst	code 0x0000 ; reset vector
 	goto start
 	
 main	code
-start	call	game_startup
-	call	interrupt_setup
-	call	startgame
+start	call	game_startup	;initialise setup and game tries (game_initialise)
+	call	interrupt_setup	;start timer 0 and enable all flag setup (interrupt)
+	call	startgame   ;send start game message to lcd (end_start)
 	
-enter	call	keyboard
-;	movlw	0xff
+enter	call	keyboard    ;to enable keypad
+;	movlw	0xff	    ;when want to avoid using keypad
 ;	CPFSEQ	PORTE
-	movlw	0xEB
-	CPFSEQ	tempo
-	bra	enter	
+	movlw	0xEB	    ;only when press E the code will procede
+	CPFSEQ	tempo	    ;tempo is the key(unstranslated) pressed
+	bra	enter	    ;loop until E is pressed in keypad
 	;call	checker
 	;movf	game_counter,W
 	;goto	$
 Initialise_sequence
-	call	LCD_Clear
+	call	LCD_Clear   ;clear the start message(lcd)
 	;Start reading the values
-	call	generate    ;generate random number
-	call	interrupt_1 ;start interrupt_1,stop interrupt_0
-	lfsr    FSR2, myinitial
-	movlw	0x04
-	movwf	read_count
-	;will remove the write at end of game
-trial_loop	
-	call	write_ans
-	decfsz	read_count
-	goto	trial_loop
-	call	count
-	
+	call	generate    ;generate random number(random_generate)
+	call	interrupt_1 ;start interrupt_1,stop interrupt_0 (interrupt)
+;	lfsr    FSR2, myinitial	;this commented code is for debug purpose to see the answer
+;	movlw	0x04
+;	movwf	read_count
+;	;will remove the write at end of game
+;trial_loop	
+;	call	write_ans
+;	decfsz	read_count
+;	goto	trial_loop
+;	call	count
+;	
 ;key in guess
 answering
-	call	keyin
-	lfsr    FSR0, myArray 
-	movf	POSTINC0,W
-	movff	PLUSW1, storage
-	movf	storage, W
-	call	LCD_Send_Byte_D	
-	movf	POSTINC0,W
-	movff	PLUSW1, storage
-	movf	storage, W
-	call	LCD_Send_Byte_D	
-	movf	POSTINC0,W
-	movff	PLUSW1, storage
-	movf	storage, W
-	call	LCD_Send_Byte_D	
-	movf	POSTINC0,W
-	movff	PLUSW1, storage
-	movf	storage, W
-	call	LCD_Send_Byte_D	
+	call	keyin		    ;call the routine to key in the guesses(
+;	lfsr    FSR0, myArray	    ;for debugging purpose to see whether the guess was able to be called correctly
+;	movf	POSTINC0,W
+;	movff	PLUSW1, storage
+;	movf	storage, W
+;	call	LCD_Send_Byte_D	
+;	movf	POSTINC0,W
+;	movff	PLUSW1, storage
+;	movf	storage, W
+;	call	LCD_Send_Byte_D	
+;	movf	POSTINC0,W
+;	movff	PLUSW1, storage
+;	movf	storage, W
+;	call	LCD_Send_Byte_D	
+;	movf	POSTINC0,W
+;	movff	PLUSW1, storage
+;	movf	storage, W
+;	call	LCD_Send_Byte_D	
 
-calculate_validate
-	call	initial	;initialization to green(yes) calculation
-	call	after_y	;from yellow light to show result in port H
-	call	end_game_logic ;consists of choosing win and lose and restart the game
-	goto	start	
+calculate_validate ;compare the guess and answer and make an end game decision(win/lose)+show result in UART/LED
+	call	initial	;from initialization to green(yes) calculation (light calculation)
+	call	after_y	;from amber light to show result in port H	(light calculation)
+	call	end_game_logic ;consists of choosing win and lose and restart the game	(end_game_seq)
+	goto	start	;restart the game
 
 ;debug purpose
 write_ans
