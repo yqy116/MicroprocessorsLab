dd99ac5bce0f0a37e617bfb6d31c4016ee55e31a
yqy116 <yqy116@skbl-422-010.ph.ic.ac.uk>
2018-11-19 12:21:37 +0000

    cleaned

--- UART.asm
+++ UART.asm
@@ -1,6 +1,6 @@
 #include p18f87k22.inc
 
-    global  UART_Setup, UART_Transmit_Message,UART_Transmit_Byte
+    global  UART_Setup,UART_Transmit_Byte
 
 acs0    udata_acs	    ; named variables in access ram
 UART_counter res 1	    ; reserve 1 byte for variable UART_counter
@@ -16,15 +16,6 @@ UART_Setup
     movlw   .103	    ; gives 9600 Baud rate (actually 9615)
     movwf   SPBRG1
     bsf	    TRISC, TX1	    ; TX1 pin as output
-    return
-
-UART_Transmit_Message	    ; Message stored at FSR2, length stored in W
-    movwf   UART_counter
-UART_Loop_message
-    movf    POSTINC2, W
-    call    UART_Transmit_Byte
-    decfsz  UART_counter
-    bra	    UART_Loop_message
     return
 
 UART_Transmit_Byte	    ; Transmits byte stored in W
